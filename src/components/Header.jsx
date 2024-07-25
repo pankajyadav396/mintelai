a951c9e4e0ceef19e079cec34528bbd28a9e9458
Your Name <you@example.com>
2024-07-25 15:58:16 +0530

    add margin to header button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 <div className="text-center max-w-[854px] mx-auto lg:pt-24 md:pt-20 pt-16 -mt-1">
                     <h1 className="uppercase md:mb-8 sm:mb-6 mb-5 font-bold font-lulo md:text-5xl sm:text-4xl text-3xl lg:text-5xxl !leading-120 text-white">Stop searching start closing</h1>
                     <p className="font-proxima-bold md:text-2xl text-xl leading-122 sm:mb-6 mb-5 md:mb-8 text-white font-bold">Helping salespeople connect to their key decision makers, automatically.</p>
-                    <button className="bg-light-green text-white rounded-lg hover:bg-white hover:text-light-green duration-300 hover:outline hover:outline-1 outline-light-green py-4 px-[29px] font-proxima-normal font-normal text-base leading-122">Early Access</button>
+                    <button className="bg-light-green text-white rounded-lg hover:bg-white hover:text-light-green duration-300 hover:outline hover:outline-1 mb-0.5 outline-light-green py-4 px-[29px] font-proxima-normal font-normal text-base leading-122">Early Access</button>
                     <div className="relative z-[2] lg:h-[550px] h-[330px] 430:h-[400px] sm:h-[450px] overflow-hidden">
                         <div className="mx-auto relative lg:max-w-[530px] overflow-hidden md:max-w-[450px] max-w-[330px] 430:max-w-[400px] ">
                             <span className="absolute z-[1] bg-light-green lg:h-[550px] h-[400px] sm:h-[500px] lg:w-[530px] w-[320px] 430:w-[400px] md:w-[450px] left-0 mix-blend-color"></span>
